d68291cf3836900464a86fccd1b37e238607f8ca
aimforkim <aimforkim@users.noreply.github.com>
2023-03-31 15:12:03 +0000

    Additions to laser_assembler

--- laser_assembler/scripts/assemble_laser.py
+++ laser_assembler/scripts/assemble_laser.py
@@ -1,18 +1,32 @@
-#!/usr/bin/env python
-import roslib
+#!/usr/bin/env python3
+
 import rospy
+from sensor_msgs.msg import PointCloud
 
 from laser_assembler.srv import *
 
-roslib.load_manifest('laser_assembler')
 
-rospy.init_node("test_client")
-rospy.wait_for_service("assemble_scans")
-try:
+def main():
+    rospy.init_node("assemble_laser", anonymous=True)
+    rospy.wait_for_service("assemble_scans")
+
+    r = rospy.Rate(1)
     assemble_scans = rospy.ServiceProxy('assemble_scans', AssembleScans)
+    pub = rospy.Publisher("/assembled_cloud", PointCloud, queue_size=1)
 
-    resp = assemble_scans(rospy.Time(5, 0), rospy.get_rostime())
-    print("Got cloud with %u points" % len(resp.cloud.points))
+    while True:
+        try:
+            resp = assemble_scans(rospy.Time(5, 0), rospy.get_rostime())
+            print("Got cloud with %u points" % len(resp.cloud.points))
+            pub.publish(resp.cloud)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s" % e)
+            break
+        r.sleep()
 
-except rospy.ServiceException as e:
-    print("Service call failed: %s" % e)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        rospy.loginfo('%s stopped' % rospy.get_name())
